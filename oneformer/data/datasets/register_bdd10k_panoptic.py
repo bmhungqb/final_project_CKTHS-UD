4074e091abff2ee52ddf75533842845d89a55211
bmhungqb <21522110@gm.uit.edu.vn>
2023-12-04 22:54:36 +0700

    hope <3

--- oneformer/data/datasets/register_bdd10k_panoptic.py
+++ oneformer/data/datasets/register_bdd10k_panoptic.py
@@ -11,20 +11,12 @@ import os
 from detectron2.data import DatasetCatalog, MetadataCatalog
 from detectron2.utils.file_io import PathManager
 
-BDD10K_CATEGORIES = []
-
 
 def load_bdd10k_panoptic_json(json_file, image_dir, gt_dir, meta):
     def _convert_category_id(segment_info, meta):
-        if segment_info["category_id"] in meta["thing_dataset_id_to_contiguous_id"]:
-            segment_info["category_id"] = meta["thing_dataset_id_to_contiguous_id"][
-                segment_info["category_id"]
-            ]
+        if str(segment_info["category_id"]) in meta["thing_dataset_id_to_contiguous_id"]:
             segment_info["isthing"] = True
         else:
-            segment_info["category_id"] = meta["stuff_dataset_id_to_contiguous_id"][
-                segment_info["category_id"]
-            ]
             segment_info["isthing"] = False
         return segment_info
 
@@ -34,9 +26,9 @@ def load_bdd10k_panoptic_json(json_file, image_dir, gt_dir, meta):
     ret = []
     for ann in json_info["annotations"]:
         image_id = ann["image_id"]
-        image_file = os.path.join(image_dir,os.path.splitext(ann["file_name"])[0] + ".jpg")
-        label_file = os.path.join(gt_dir,ann["file_name"])
-        segments_info = [_convert_category_id(x,meta) for x in ann["segments_info"]]
+        image_file = os.path.join(image_dir, os.path.splitext(ann["file_name"])[0] + ".jpg")
+        label_file = os.path.join(gt_dir, ann["file_name"])
+        segments_info = [_convert_category_id(x, meta) for x in ann["segments_info"]]
         ret.append(
             {
                 "file_name": image_file,
@@ -50,8 +42,9 @@ def load_bdd10k_panoptic_json(json_file, image_dir, gt_dir, meta):
     assert PathManager.isfile(ret[0]["pan_seg_file_name"]), ret[0]["pan_seg_file_name"]
     return ret
 
+
 def register_bdd10k_panoptic(
-    name, image_root, panoptic_root, panoptic_json
+        name, image_root, panoptic_root, panoptic_json
 ):
     panoptic_name = name
     with PathManager.open(panoptic_json) as f:
@@ -64,11 +57,11 @@ def register_bdd10k_panoptic(
     MetadataCatalog.get("bdd10k_sem_seg_val").set(
         stuff_colors=BDD10K_COLORS[:],
     )
-    metadata = get_metadata()
+    metadata = get_metadata(BDD10K_CATEGORIES)
 
     DatasetCatalog.register(
         panoptic_name,
-        lambda : load_bdd10k_panoptic_json(
+        lambda: load_bdd10k_panoptic_json(
             panoptic_json, image_root, panoptic_root, metadata
         ),
     )
@@ -82,11 +75,12 @@ def register_bdd10k_panoptic(
         label_divisor=1000,
         **metadata,
     )
-    
+
+
 _PREDEFINED_SPLITS_BDD10K_PANOPTIC = {
     "bdd10k_panoptic_train": (
         "bdd10k/images/train",
-        "bdd10k/labels/pan_seg/bitmasks/train",
+        "bdd10k/labels/jsons/pan_seg/masks/train",
         "bdd10k/labels/jsons/pan_seg_train_cocofmt.json",
     ),
     "bdd10k_panoptic_val": (
@@ -97,10 +91,10 @@ _PREDEFINED_SPLITS_BDD10K_PANOPTIC = {
 }
 
 
-def get_metadata():
+def get_metadata(BDD10K_CATEGORIES):
     meta = {}
-    thing_classes = [k["name"] for k in BDD10K_CATEGORIES if k["isthing"] == 1]
-    thing_colors = [k["color"] for k in BDD10K_CATEGORIES if k["isthing"] == 1]
+    thing_classes = [k["name"] for k in BDD10K_CATEGORIES if k["isthing"] == True]
+    thing_colors = [k["color"] for k in BDD10K_CATEGORIES if k["isthing"] == True]
     stuff_classes = [k["name"] for k in BDD10K_CATEGORIES]
     stuff_colors = [k["color"] for k in BDD10K_CATEGORIES]
 
@@ -114,22 +108,23 @@ def get_metadata():
 
     for i, cat in enumerate(BDD10K_CATEGORIES):
         if cat["isthing"]:
-            thing_dataset_id_to_contiguous_id[cat["id"]] = i
-        # else:
-        #     stuff_dataset_id_to_contiguous_id[cat["id"]] = i
+            thing_dataset_id_to_contiguous_id[str(cat["id"])] = i
+        else:
+            stuff_dataset_id_to_contiguous_id[str(cat["id"])] = i
 
         # in order to use sem_seg evaluator
-        stuff_dataset_id_to_contiguous_id[cat["id"]] = i
+    #         stuff_dataset_id_to_contiguous_id[str(cat["id"])] = i
 
     meta["thing_dataset_id_to_contiguous_id"] = thing_dataset_id_to_contiguous_id
     meta["stuff_dataset_id_to_contiguous_id"] = stuff_dataset_id_to_contiguous_id
 
     return meta
 
+
 def register_all_bdd10k_panoptic(root):
     for (
-        prefix,
-        (image_root, panoptic_root, panoptic_json),
+            prefix,
+            (image_root, panoptic_root, panoptic_json),
     ) in _PREDEFINED_SPLITS_BDD10K_PANOPTIC.items():
         register_bdd10k_panoptic(
             prefix,
@@ -137,6 +132,7 @@ def register_all_bdd10k_panoptic(root):
             os.path.join(root, panoptic_root),
             os.path.join(root, panoptic_json),
         )
+
 
 _root = os.getenv("", "datasets")
 register_all_bdd10k_panoptic(_root)
